30e5063589c50e663f8881bd3d8925ef23db998a
Kaustubh <kaustubhkhavnekar@gmail.com>
2019-03-17 04:55:33 +0530

    Added filter API url

--- api/urls.py
+++ api/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('get/scholarship/organisation/', views.get_scholarship_organisation, name='get_scholarship_organisation'),
     path('get/scholarship/details/', views.get_scholarship_details, name='get_scholarship_details'),
     path('get/scholarship/application/', views.get_scholarship_application, name='get_scholarship_application'),
+    path('get/scholarship/filter/', views.get_scholarship_filter, name='get_scholarship_filter'),
     path('post/scholarship/apply/', views.post_scholarship_apply, name='post_scholarship_apply'),
     path('post/application/status/', views.post_application_status, name='post_application_status'),
 ]
